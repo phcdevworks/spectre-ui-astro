8f7cbd1fedd12a1b30f84934107bdf4115bffb33
Bradley J Potts <brad.potts@coastdigitalgroup.com>
2025-12-14 02:03:35 +0800

    Re-export all types from types module
    Added a re-export statement to export all types from the './types' module, making them available to consumers of the package.

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,4 @@ export { default as SpButton } from "./components/SpButton.astro";
 export { default as SpCard } from "./components/SpCard.astro";
 export { default as SpInput } from "./components/SpInput.astro";
 export { SPECTRE_UI_CSS } from "./styles";
+export type * from "./types";
